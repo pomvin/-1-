09119320c61a319a0204552212880de203ddaf93
«Maria» <chalova_2111@mail.ru>
2023-03-24 11:04:52 +0500

    2

---
+++ Task_2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, 
+// какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.WriteLine("Введите число А: ");
+int A = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число В: ");
+int B = int.Parse(Console.ReadLine()!);
+
+if (A > B)
+{
+    Console.WriteLine($"Максимальное число {A}");
+}
+else
+{
+    Console.WriteLine($"Максимальное число {B}");
+}
---
+++ Task_4/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход 
+// три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine("Введите три числа : ");
+int A, B, C;
+A = Convert.ToInt32(Console.ReadLine());
+B = Convert.ToInt32(Console.ReadLine());
+C = Convert.ToInt32(Console.ReadLine());
+
+if (A >= B)
+{
+    if (A >= C);
+    {
+        Console.WriteLine($"Максимальное число {A}");
+    }
+}
+else if (C >= B)
+{
+    Console.WriteLine($"Максимально число {C}");
+}
+else
+{
+    Console.WriteLine($"Максимальное число {B}");
+}
+
+
